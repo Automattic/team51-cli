1ea84031a11cd4fcebd216b8a77bb49d65e76e01
Spencer Cameron-Morin <pippercameron@gmail.com>
2020-05-11 10:38:31 -0500

    Regenerate autoload files.
    This avoids breaking the tool when a new command/class is added.

--- team51-cli.php
+++ team51-cli.php
@@ -3,6 +3,8 @@
 
 echo "Checking for updates.." . PHP_EOL;
 exec( sprintf( "git -C %s %s",  __DIR__, 'pull' ) );
+// TODO: Only run this when there are updates.
+exec( "composer dump-autoload -o" );
 echo PHP_EOL;
 
 require __DIR__ . '/load-application.php';
