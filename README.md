46e7a2d2ed9b4be43389e48fe9dae5351a20adcb
Esteban Cairol <ecairol@gmail.com>
2022-04-21 15:01:19 -0600

    Onboard Github function changed to use REST API instead of gh cli, to avoid extra dependencies.
    Subscribes a given username to one of 3 Team access levels
    
    Remove type parameter and replaced it with github_team

--- README.md
+++ README.md
@@ -28,7 +28,6 @@ Be well, be kind, make things and set them free.
 
 ## Dependencies
 - Composer (instructions below)
-- GitHub CLI (`gh`) for certaing user related tasks
 
 ## Installation
 1. Open the Terminal on your Mac and clone this repository by running:
@@ -62,6 +61,9 @@ If you get the error `./install-osx: line 2: composer: command not found`, you c
 
 ### `brew: command not found`
 If you don't have [brew](https://brew.sh/) yet, install it by executing this from your Terminal: `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"` — Tip: you can use brew to install all sort of apps on your Mac. Give it [a try](https://formulae.brew.sh/cask/zoom)
+
+### `gh: command not found`
+If you're onboarding/offboarding users, you will need access to Github CLI. Please refer to the [installation instructions](https://cli.github.com/), and then follow the instructions on the Terminal for authentication.
 
 ### `Warning: file_get_contents: failed to open stream`
 ```
--- src/commands/onboard-collaborator.php
+++ src/commands/onboard-collaborator.php
@@ -13,32 +13,27 @@ class Onboard_Collaborator extends Command {
 	private $api_helper;
 	private $output;
 
+	const ACCESS_1 = 'access-level-1';
+	const ACCESS_2 = 'access-level-2';
+	const ACCESS_3 = 'access-level-3';
+
+
 	protected function configure() {
 		$this
 		->setDescription( 'Adds collaborator to all Github repos and Pressable sites. Collaborator can be either a11n or contractor' )
 		->setHelp( 'This command allows you to bulk add a collabortor to all Pressable sites and Github repos.' )
-		->addOption( 'type', null, InputOption::VALUE_REQUIRED, 'Type of collaborator. Either a11n or contractor.' )
 		->addOption( 'email', null, InputOption::VALUE_REQUIRED, "Collaborator's email." )
 		->addOption( 'github', null, InputOption::VALUE_REQUIRED, "Collaborator's Github username." )
-		->addOption( 'github_team', null, InputOption::VALUE_NONE, 'Optional Github team for this collaborator.' );
+		->addOption( 'github_team', null, InputOption::VALUE_REQUIRED, sprintf('Github team can be: %s, %s, or %s. Following this order, level 1 has less privileges than level 3', self::ACCESS_1, self::ACCESS_2, self::ACCESS_3) );
 	}
 
 	protected function execute( InputInterface $input, OutputInterface $output ) {
 		$this->api_helper = new API_Helper();
 		$this->output     = $output;
 
-		$type = $input->getOption( 'type' );
-		if ( empty( $type ) ) {
-			$type = trim( readline( 'Please provide the type of collaborator. Enter "a11n" or "contractor": ' ) );
-			if ( empty( $type ) ) {
-				$output->writeln( '<error>Missing collaborator type (eg: --type=contractor).</error>' );
-				exit;
-			}
-		}
-
 		$email = $input->getOption( 'email' );
 		if ( empty( $email ) ) {
-			$email = trim( readline( 'Please provide the email of the collaborator: ' ) );
+			$email = trim( readline( "Please provide the collaborator's email: " ) );
 			if ( empty( $email ) ) {
 				$output->writeln( "<error>Missing collaborator's email (eg: --email=user@domain.com).</error>" );
 				exit;
@@ -47,7 +42,7 @@ class Onboard_Collaborator extends Command {
 
 		$github_user = $input->getOption( 'github' );
 		if ( empty( $github_user ) ) {
-			$github_user = trim( readline( 'Please provide the Github username of the collaborator: ' ) );
+			$github_user = trim( readline( "Please provide the collaborator's Github username : " ) );
 			if ( empty( $github_user ) ) {
 				$github_user->writeln( "<error>Missing collaborator's Github username (eg: --github=their_username).</error>" );
 				exit;
@@ -55,26 +50,68 @@ class Onboard_Collaborator extends Command {
 		}
 
 		$github_team = $input->getOption( 'github_team' );
+		if ( empty( $github_team ) || ! in_array($github_team, [self::ACCESS_1, self::ACCESS_2, self::ACCESS_3]) ) {
+			$github_team = trim( readline( sprintf("Please provide the collaborator's Github team. Values can be: %s, %s, or %s: ", self::ACCESS_1, self::ACCESS_2, self::ACCESS_3) ) );
+			if ( empty( $github_team ) || ! in_array($github_team, [self::ACCESS_1, self::ACCESS_2, self::ACCESS_3]) ) {
+				$output->writeln( '<error>Missing collaborator github_team (eg: --github_team='.self::ACCESS_1.').</error>' );
+				exit;
+			}
+		}
 
 		// Start process
-		$this->onboard_github( $output, $github_user, $github_team );
+		$this->onboard_github( $github_user, $github_team );
 
 
 		$output->writeln( '<info>All done!<info>' );
 	}
 
 
-	private function onboard_github( $output, $gh_username ) {
-		$output->writeln( '<comment>Adding collaborator to a8cteam51 Github repos...</comment>' );
-		$repo_names = exec( 'gh repo list a8cteam51 --no-archived --limit 1000 --json name' );
-		$repo_names = json_decode( $repo_names );
+	private function onboard_github( $gh_username, $gh_team ) {
+		$this->output->writeln( '<comment>Pulling list of Github repos for organzation a8cteam51...</comment>' );
 
-		foreach( $repo_names as $repo_name ) {
-			$repo_name = $repo_name->name;
-			$output->writeln( '<comment>'.$repo_name.'</comment>' );
-			$command = "gh api repos/a8cteam51/$repo_name/collaborators/$gh_username -X PUT -f permission='push'";
-			passthru( $command );
+		$page = 1;
+		$load_more = 1;
+		$repo_names = array();
+
+		// Load all Github repositories
+		while ($load_more > 0) {
+			$tmp_repos = $this->api_helper->call_github_api(
+				sprintf( 'orgs/%s/repos?per_page=100&page=%s', GITHUB_API_OWNER, $page ),
+				'',
+				'GET'
+			);
+			$this->output->writeln( '<comment>...</comment>' );
+			$tmp_repo_names = array_map( function( $row ) {
+				return $row->name;
+			}, $tmp_repos);
+
+			$repo_names = array_merge( $repo_names, $tmp_repo_names );
+			$load_more  = count( $tmp_repos );
+			$page++;
 		}
-		$output->writeln( '<comment>All set for our Github repos.</comment>' );
+
+		$this->output->writeln( '<comment>Found ' . count($repo_names) . ' repositories.</comment>' );
+
+		// Associate Github username with Github Team
+		$team_put = $this->api_helper->call_github_api(
+			sprintf( 'orgs/%s/teams/%s/memberships/%s', GITHUB_API_OWNER, $gh_team, $gh_username ),
+			array(
+				'role' => 'member'
+			),
+			'PUT'
+		);
+
+		// TODO: Do we still need this loop to grant access per-repo?
+		foreach( $repo_names as $repo_name ) {
+			// $output->writeln( '<comment>'.$repo_name.'</comment>' );
+			// $tmp_repos = $this->api_helper->call_github_api(
+			// 	sprintf( 'orgs/%s/repos?per_page=100&page=%s', GITHUB_API_OWNER, $page ),
+			// 	array(
+			// 		'permission': 'push'
+			// 	),
+			// 	'PUT'
+			// );
+		}
+		$this->output->writeln( '<comment>All set for our Github repos.</comment>' );
 	}
 }
