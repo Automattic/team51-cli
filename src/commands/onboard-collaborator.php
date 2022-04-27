c7fb6afd1b873d39908a106fe9ed242ddf429c00
Esteban Cairol <ecairol@gmail.com>
2022-04-27 15:36:55 -0600

    Changed teams name to be Triage, Deploy and Admin.
    Clean-up code related to calling all repos and giving individual access to each repo. This is all now managed at Team level

--- src/commands/onboard-collaborator.php
+++ src/commands/onboard-collaborator.php
@@ -13,17 +13,17 @@ class Onboard_Collaborator extends Command {
 	private $api_helper;
 	private $output;
 
-	const ACCESS_1 = 'access-level-1';
-	const ACCESS_2 = 'access-level-2';
-	const ACCESS_3 = 'access-level-3';
+	const ACCESS_1 = 'triage';
+	const ACCESS_2 = 'deploy';
+	const ACCESS_3 = 'admin';
 
 
 	protected function configure() {
 		$this
-		->setDescription( 'Adds collaborator to all Github repos and Pressable sites. Collaborator can be either a11n or contractor' )
-		->setHelp( 'This command allows you to bulk add a collabortor to all Pressable sites and Github repos.' )
+		->setDescription( 'Adds collaborator to Github Team and Pressable sites. Collaborator can be either a11n or contractor' )
+		->setHelp( 'This command allows you to bulk add a collaborator to all Pressable sites and a Github team.' )
 		->addOption( 'email', null, InputOption::VALUE_REQUIRED, "Collaborator's email." )
-		->addOption( 'github', null, InputOption::VALUE_REQUIRED, "Collaborator's Github username." )
+		->addOption( 'github_username', null, InputOption::VALUE_REQUIRED, "Collaborator's Github username." )
 		->addOption( 'github_team', null, InputOption::VALUE_REQUIRED, sprintf('Github team can be: %s, %s, or %s. Following this order, level 1 has less privileges than level 3', self::ACCESS_1, self::ACCESS_2, self::ACCESS_3) );
 	}
 
@@ -31,16 +31,16 @@ class Onboard_Collaborator extends Command {
 		$this->api_helper = new API_Helper();
 		$this->output     = $output;
 
-		$email = $input->getOption( 'email' );
-		if ( empty( $email ) ) {
-			$email = trim( readline( "Please provide the collaborator's email: " ) );
-			if ( empty( $email ) ) {
-				$output->writeln( "<error>Missing collaborator's email (eg: --email=user@domain.com).</error>" );
-				exit;
-			}
-		}
+		// $email = $input->getOption( 'email' );
+		// if ( empty( $email ) ) {
+		// 	$email = trim( readline( "Please provide the collaborator's email: " ) );
+		// 	if ( empty( $email ) ) {
+		// 		$output->writeln( "<error>Missing collaborator's email (eg: --email=user@domain.com).</error>" );
+		// 		exit;
+		// 	}
+		// }
 
-		$github_user = $input->getOption( 'github' );
+		$github_user = $input->getOption( 'github_username' );
 		if ( empty( $github_user ) ) {
 			$github_user = trim( readline( "Please provide the collaborator's Github username : " ) );
 			if ( empty( $github_user ) ) {
@@ -61,36 +61,15 @@ class Onboard_Collaborator extends Command {
 		// Start process
 		$this->onboard_github( $github_user, $github_team );
 
+		// TODO: onboard_pressable()
+
 
 		$output->writeln( '<info>All done!<info>' );
 	}
 
 
 	private function onboard_github( $gh_username, $gh_team ) {
-		$this->output->writeln( '<comment>Pulling list of Github repos for organzation a8cteam51...</comment>' );
-
-		$page = 1;
-		$load_more = 1;
-		$repo_names = array();
-
-		// Load all Github repositories
-		while ($load_more > 0) {
-			$tmp_repos = $this->api_helper->call_github_api(
-				sprintf( 'orgs/%s/repos?per_page=100&page=%s', GITHUB_API_OWNER, $page ),
-				'',
-				'GET'
-			);
-			$this->output->writeln( '<comment>...</comment>' );
-			$tmp_repo_names = array_map( function( $row ) {
-				return $row->name;
-			}, $tmp_repos);
-
-			$repo_names = array_merge( $repo_names, $tmp_repo_names );
-			$load_more  = count( $tmp_repos );
-			$page++;
-		}
-
-		$this->output->writeln( '<comment>Found ' . count($repo_names) . ' repositories.</comment>' );
+		$this->output->writeln( "<comment>Granting access to our a8cteam51 organization...</comment>" );
 
 		// Associate Github username with Github Team
 		$team_put = $this->api_helper->call_github_api(
@@ -101,17 +80,10 @@ class Onboard_Collaborator extends Command {
 			'PUT'
 		);
 
-		// TODO: Do we still need this loop to grant access per-repo?
-		foreach( $repo_names as $repo_name ) {
-			// $output->writeln( '<comment>'.$repo_name.'</comment>' );
-			// $tmp_repos = $this->api_helper->call_github_api(
-			// 	sprintf( 'orgs/%s/repos?per_page=100&page=%s', GITHUB_API_OWNER, $page ),
-			// 	array(
-			// 		'permission': 'push'
-			// 	),
-			// 	'PUT'
-			// );
+		if ( ! empty( $team_put->message ) ) {
+			$this->output->writeln( "<error>Something went wrong. Github says: {$team_put->message}</error>" );
+		} else {
+			$this->output->writeln( "<info>The user '{$gh_username}' has been added to Team '{$gh_team}'!</info>" );
 		}
-		$this->output->writeln( '<comment>All set for our Github repos.</comment>' );
 	}
 }
