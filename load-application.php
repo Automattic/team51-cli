dc12c63917afeea023ae0af85d56b17bfc80c3a0
Nicholas Pagazani <nick.pagazani@automattic.com>
2022-05-20 13:37:59 -0400

    Resolve merge conflicts

--- load-application.php
+++ load-application.php
@@ -27,6 +27,11 @@ $application->add( new Team51\Command\Front_Get_Export() );
 $application->add( new Team51\Command\Get_PHP_Errors() );
 $application->add( new Team51\Command\Remove_User() );
 $application->add( new Team51\Command\DevQueue_Triage_Digest() );
+$application->add( new Team51\Command\Update_Repository_Secret() );
+$application->add( new Team51\Command\Plugin_List() );
+$application->add( new Team51\Command\Pressable_Generate_Token() );
+$application->add( new Team51\Command\Pressable_Grant_Access() );
 $application->add( new Team51\Command\Plugin_Search() );
+
 
 $application->run();
