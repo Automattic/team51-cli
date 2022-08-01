cbf8d132238f77e40f86cdd7f476ad390c4e4016
Mike Straw <mike.straw@automattic.com>
2022-08-01 14:51:52 -0400

    Add Safety Net as a submodule

--- team51-cli.php
+++ team51-cli.php
@@ -3,13 +3,13 @@
 
 // Respect -q and --quiet.
 if ( ! in_array( '-q', $argv ) && ! in_array( '--quiet', $argv ) ) {
-    echo "Checking for updates.." . PHP_EOL;
+	echo 'Checking for updates..' . PHP_EOL;
 }
 
-exec( sprintf( "git -C %s %s",  __DIR__, 'pull' ) );
+exec( sprintf( 'git -C %s %s --recurse-submodules=yes', __DIR__, 'pull' ) );
 // TODO: Only run this when there are updates.
-exec( sprintf( "composer install -o --working-dir %s", __DIR__ ) );
-exec( sprintf( "composer dump-autoload -o --working-dir %s", __DIR__ ) );
+exec( sprintf( 'composer install -o --working-dir %s', __DIR__ ) );
+exec( sprintf( 'composer dump-autoload -o --working-dir %s', __DIR__ ) );
 echo PHP_EOL;
 
 require __DIR__ . '/load-application.php';
