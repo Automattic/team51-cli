a67a2bd1c8dececcab997c8df6b260d6ba53bbee
Mike Straw <mike.straw@automattic.com>
2022-08-03 12:39:15 -0400

    Add plugin to set dev environment

---
+++ scaffold/develop/set-develop-environment.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Team 51 Set Development Environment
+Plugin URI: https://github.com/Automattic/team51-cli/blob/trunk/scaffold/templates/set-develop-environment.php
+Description: Sets the WP environment to development.
+Version: 1.0.0
+Author: WordPress.com Special Projects / Mike Straw
+Author URI: https://wpspecialprojects.wordpress.com/
+License: GPLv3
+*/
+
+define( 'WP_ENVIRONMENT_TYPE', 'development' );
