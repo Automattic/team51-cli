1525eef8924a51761416a530a7d44fb9b3df1bd1
Antonius Hegyes <tony.hegyes@automattic.com>
2022-11-01 16:43:54 -0600

    Remove superfluous import

--- src/commands/pressable-site-open-shell.php
+++ src/commands/pressable-site-open-shell.php
@@ -3,7 +3,6 @@
 namespace Team51\Command;
 
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
