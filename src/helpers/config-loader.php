63e7d396b9a274c6bb05b7c32483c72fb92f7080
Tiago Noronha <hello@tiagonoronha.com>
2022-11-11 16:35:16 +0000

    Drop 1PW requirement for contractors

--- src/helpers/config-loader.php
+++ src/helpers/config-loader.php
@@ -10,36 +10,33 @@ foreach ( $legacy_config_files as $legacy_config_file ) {
 	}
 }
 
-// Generate the config file using the 1Password data.
-$config_file = TEAM51_CLI_ROOT_DIR . '/secrets/config.json';
-if ( \file_exists( $config_file ) ) {
+// Generate the config file using the 1Password data or contractor specific credentials.
+$config = array();
+
+if ( ! is_contractor_mode() ) {
+	$config_file = TEAM51_CLI_ROOT_DIR . '/secrets/config.json';
+	if ( \file_exists( $config_file ) ) {
+		\unlink( $config_file );
+	}
+
+	$template_file = TEAM51_CLI_ROOT_DIR . '/secrets/config.tpl.json';
+
+	$result = \exec( \sprintf( 'op inject -i %1$s -o %2$s', $template_file, $config_file ) );
+	if ( empty( $result ) ) {
+		echo "\033[31mThere was an error generating the config file.\033[0m If the line above contains 'command not found', you likely need to upgrade to 1Password 8 and install the accompanying CLI tool." . PHP_EOL;
+		echo "\033[36mPlease refer to the README for instructions on doing that and for the solutions to other common errors.\033[0m" . PHP_EOL;
+		exit( 1 );
+	}
+
+	// Parse the config file.
+	$config = decode_json_content( \file_get_contents( $config_file ), true );
+
+	// Remove the config file (we don't need it anymore).
 	\unlink( $config_file );
-}
-
-$template_file = TEAM51_CLI_ROOT_DIR . '/secrets/config.tpl.json';
-if ( is_contractor_mode() ) {
-	$template_file = TEAM51_CLI_ROOT_DIR . '/secrets/config__contractors.tpl.json';
-}
-
-$result = \exec( \sprintf( 'op inject -i %1$s -o %2$s', $template_file, $config_file ) );
-if ( empty( $result ) ) {
-	echo "\033[31mThere was an error generating the config file.\033[0m If the line above contains 'command not found', you likely need to upgrade to 1Password 8 and install the accompanying CLI tool." . PHP_EOL;
-	echo "\033[36mPlease refer to the README for instructions on doing that and for the solutions to other common errors.\033[0m" . PHP_EOL;
-	exit( 1 );
-}
-
-// Parse the config file.
-$config = decode_json_content( \file_get_contents( $config_file ), true );
-if ( empty( $config ) ) {
-	exit( 'Config file could not be read or it is empty. Please make sure it is properly formatted. Aborting!' . PHP_EOL );
-}
-
-// Maybe parse contractor config file.
-if ( is_contractor_mode() ) {
+} else {
 	$contractor_config_file = TEAM51_CLI_ROOT_DIR . '/secrets/config__contractors.json';
 	if ( \file_exists( $contractor_config_file ) ) {
-		$contractor_config = decode_json_content( \file_get_contents( $contractor_config_file ), true ) ?: array();
-		$config            = \array_merge_recursive( $config, $contractor_config );
+		$config = decode_json_content( \file_get_contents( $contractor_config_file ), true ) ?: array();
 	}
 }
 
@@ -48,6 +45,10 @@ $overwrite_config_file = TEAM51_CLI_ROOT_DIR . '/secrets/config.overwrite.json';
 if ( \file_exists( $overwrite_config_file ) ) {
 	$overwrite_config = decode_json_content( \file_get_contents( $overwrite_config_file ), true ) ?: array();
 	$config           = \array_replace_recursive( $config, $overwrite_config );
+}
+
+if ( empty( $config ) ) {
+	exit( 'Config file could not be read or it is empty. Please make sure it is properly formatted. Aborting!' . PHP_EOL );
 }
 
 // Register secrets as constants.
@@ -61,6 +62,3 @@ foreach ( $config as $section => $secrets ) {
 		\define( $constant_name, $secret );
 	}
 }
-
-// Remove the config file (we don't need it anymore).
-\unlink( $config_file );
