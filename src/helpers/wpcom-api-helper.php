17ee855fd5beb860936bc5041b6f4eb9c787c0fa
Antonius Hegyes <tony.hegyes@automattic.com>
2022-12-12 15:08:41 +0100

    Better DNS caching logging

--- src/helpers/wpcom-api-helper.php
+++ src/helpers/wpcom-api-helper.php
@@ -2,6 +2,7 @@
 
 namespace Team51\Helper;
 
+use Amp\Dns\Record;
 use Amp\Http\Client\HttpClientBuilder;
 use Amp\Http\Client\Request;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -75,7 +76,13 @@ final class WPCOM_API_Helper {
 	 * @throws  \Throwable  If an error occurs in a request promise.
 	 */
 	public static function call_api_concurrent( array $endpoints, string $method = 'GET', array $params = array() ): array {
-		wait( resolve( parse_url( self::BASE_URL, PHP_URL_HOST ) ) ); // Prime the DNS cache.
+		console_writeln( 'Calling WordPress.com API concurrently...' );
+
+		$result = wait( resolve( parse_url( self::BASE_URL, PHP_URL_HOST ), Record::A ) ); // Prime the DNS cache.
+		var_dump( $result );
+
+		console_writeln( 'DNS cache primed.' );
+		sleep( 5 );
 
 		$http_client = HttpClientBuilder::buildDefault();
 		$promises    = array();
