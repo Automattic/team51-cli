01672eb9c2fd8058ee8d04c13cf272632dbfef4d
Nicholas Pagazani <nick.pagazani@automattic.com>
2023-07-27 22:03:42 -0400

    Fix file content API call

--- src/commands/fse-starter-themes.php
+++ src/commands/fse-starter-themes.php
@@ -66,9 +66,23 @@ class FSE_Starter_Themes extends Command {
 						$inc_patterns_exists = true;
 					}
 					if ( 'style.css' === $file->name ) {
-						var_dump( $file->url );
-						$css_content = GitHub_API_Helper::call_api( $file->url, 'GET' );
-						var_dump( $css_content );
+						$css_content_response = $this->call_github_api( $file->url );
+
+						if ( isset( $css_content_response->content ) ) {
+							$css_content = base64_decode( $css_content_response->content ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_decode
+						}
+
+						if ( isset( $css_content ) ) {
+							// Match "Theme Name:"
+							if ( preg_match( '/Theme Name:\s*(.*)/i', $css_content, $matches ) ) {
+								$theme_name = trim( $matches[1] );
+							}
+
+							// Match "Theme URI:"
+							if ( preg_match( '/Theme URI:\s*(.*)/i', $css_content, $matches ) ) {
+								$theme_uri = trim( $matches[1] );
+							}
+						}
 
 						if ( preg_match( '/Template:\s*(.*)/i', $css_content, $matches ) ) {
 							$empty_template_value = trim( $matches[1] ) === '';
@@ -78,10 +92,49 @@ class FSE_Starter_Themes extends Command {
 
 				if ( $theme_json_exists && ! $inc_patterns_exists && $empty_template_value ) {
 					$output->writeln( $theme->name, OutputInterface::VERBOSITY_NORMAL );
+					$output->writeln( $theme_uri, OutputInterface::VERBOSITY_NORMAL );
+					$output->writeln( $theme_name, OutputInterface::VERBOSITY_NORMAL );
 				}
 			}
 		}
 		return 0;
+	}
+
+	protected function call_github_api( string $url ): ?object {
+		$ch = curl_init(); // phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_init
+
+		curl_setopt_array( // phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt_array
+			$ch,
+			array(
+				CURLOPT_URL            => $url,
+				CURLOPT_HTTPHEADER     => array(
+					'Accept: application/vnd.github+json',
+					'Content-Type: application/json',
+					'Authorization: Bearer ' . GITHUB_API_TOKEN,
+					'X-GitHub-Api-Version: 2022-11-28',
+					'User-Agent: PHP',
+				),
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_HEADER         => false,
+				CURLOPT_FAILONERROR    => true,
+				CURLOPT_ENCODING       => '',
+				CURLOPT_MAXREDIRS      => 10,
+				CURLOPT_TIMEOUT        => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST  => 'GET',
+			)
+		);
+
+		$response = curl_exec( $ch ); // phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_exec
+
+		if ( curl_errno( $ch ) ) { // phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_errno
+			echo 'Error:' . curl_error( $ch ); // phpcs:ignore WordPress.WP.AlternativeFunctions.curl, WordPress.Security.EscapeOutput.OutputNotEscaped_curl_error
+		}
+
+		curl_close( $ch ); // phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_close
+
+		return json_decode( $response ) ? json_decode( $response ) : null;
 	}
 
 }
